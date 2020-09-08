ce75d8bcddc6291e463098398067278446c8d827
AlejandraL205 <62623963+AlejandraL205@users.noreply.github.com>
2020-09-07 19:25:46 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -16,18 +16,3 @@ GREEN = (   0, 255,   0)
 RED   = ( 255,   0,   0)
 BLUE  = (   0,   0, 255)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-            
-    #fondo
-    drawSurface.fill(BLACK)
-    
-    #dibujar objetos
-    pygame.draw.rect(drawSurface, RED, [0, 0, 16, 16])
-    
-    #actualizar
-    pygame.transform.scale(drawSurface, screenSize, screen)
-    pygame.display.flip()
-    clock.tick(60)
